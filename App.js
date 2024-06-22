a3a01c66dfab3e2db376f28caa0733d55e9c143e
Anusha <anushasaleem.dev@gmail.com>
2024-06-21 19:34:07 -0700

    Profile pic title screen made

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppNavigation from './src/Navigation';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
+
+const loadFonts = () => {
+  return Font.loadAsync({
+    'Hi-Melody': require('./assets/fonts/HiMelody-Regular.ttf'),
+  });
+};
 
 export default function App() {
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
        <AppNavigation />
   );
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,6 +1,8 @@
-import { View, ScrollView } from 'react-native'
+import { View, ScrollView, Text, TouchableOpacity, Image, Touchable } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
 
 export default function HomeScreen() {
   return (
@@ -11,6 +13,13 @@ export default function HomeScreen() {
         contentContainerStyle={{paddingBottom: 50}}
         className="space-y-6 pt-14"
         >
+            {/*Profile*/}
+            <View className="mx-4 flex-row justify-between items-center mb-2">
+                <Image 
+                source={require('../../assets/p_cat.jpeg')}
+                style={{width: hp(10), height: hp(10)}} 
+                className="rounded-full mt-3" />
+            </View>
             
         </ScrollView>
     </View>
