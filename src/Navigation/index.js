1b592a96d223ac26b2a660dde7e94c69af918e5c
Anusha <anushasaleem.dev@gmail.com>
2024-07-18 12:44:39 -0700

    added basic bottom nav

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,19 +1,38 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TitleScreen from '../Screens/TitleScreen';
-import HomeScreen from '../Screens/HomeScreen';
 import CreateScreen from '../Screens/CreateScreen';
-import React from 'react'
+import HomeScreen from '../Screens/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function AppNavigation() {
   return (
-      <Stack.Navigator initialRouteName="Title" screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Title' component={TitleScreen} />
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Create' component={CreateScreen} />
-      </Stack.Navigator>
-  
+    <Tab.Navigator 
+      initialRouteName="Home" 
+      activeColor='#FFC107'
+    >
+      <Tab.Screen 
+        name='Home' 
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }} 
+      />
+      <Tab.Screen 
+        name='Create' 
+        component={CreateScreen}
+        options={{
+          tabBarLabel: 'Create',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="plus-circle" color={color} size={26} />
+          ),
+        }} 
+      />
+    </Tab.Navigator>
   );
 }
--- src/Screens/CreateScreen.js
+++ src/Screens/CreateScreen.js
@@ -1,10 +1,13 @@
-import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
 
 export default function CreateScreen() {
   return (
     <SafeAreaView>
-      <Text>CreateScreen</Text>
+      {/* Select Ingredients */}
+      <Text>Select Ingredients</Text>
+      {/* Add Ingredients */}
+
     </SafeAreaView>
   )
 }
